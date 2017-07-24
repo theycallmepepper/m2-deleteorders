3346c876d2ca1c32a8c6ecca6ff7dc363cbd5e61
David Young <davidaaronyoung@gmail.com>
2017-07-23 19:48:42 -0700

    Refactored block to plugin.

--- app/code/TCMP/DeleteOrders/Plugin/Block/Adminhtml/Order/View.php
+++ app/code/TCMP/DeleteOrders/Plugin/Block/Adminhtml/Order/View.php
@@ -3,16 +3,26 @@
 namespace TCMP\DeleteOrders\Plugin\Block\Adminhtml\Order;
 
 
-use Magento\Sales\Block\Adminhtml\Order\View as OGView;
+class View {
 
+	protected $_authorization;
 
-class View extends OGView {
+	protected $_urlBuilder;
+
+	protected $_request;
+
+	public function __construct( \Magento\Backend\Block\Template\Context $context, array $data = [] )
+	{
+		$this->_authorization = $context->getAuthorization();
+		$this->_urlBuilder    = $context->getUrlBuilder();
+		$this->_request       = $context->getRequest();
+	}
 
 	public function beforeSetLayout( \Magento\Sales\Block\Adminhtml\Order\View $view )
 	{
 		if ( $this->_isAllowedAction( 'TCMP_DeleteOrders::massDelete' ) ) {
 			$message = __( 'Are you sure you want to delete the order?' );
-			$this->buttonList->add(
+			$view->addButton(
 				'order_delete',
 				[
 					'label'   => __( 'Delete' ),
@@ -20,5 +30,38 @@ class View extends OGView {
 				]
 			);
 		}
+	}
+
+	/**
+	 * Check permission for passed action
+	 *
+	 * @param string $resourceId
+	 *
+	 * @return bool
+	 */
+	protected function _isAllowedAction( $resourceId )
+	{
+		return $this->_authorization->isAllowed( $resourceId );
+	}
+
+
+	/**
+	 * Generate url by route and parameters
+	 *
+	 * @param   string $route
+	 * @param   array  $params
+	 *
+	 * @return  string
+	 */
+	public function getUrl( $route = '', $params = [] )
+	{
+		$params['order_id'] = $this->getOrderId();
+
+		return $this->_urlBuilder->getUrl( $route, $params );
+	}
+
+	private function getOrderId()
+	{
+		return $this->_request->getParam( 'order_id' );
 	}
 }
